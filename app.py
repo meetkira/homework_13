496ba58319b66fbdd217f6576f65346ced2ff8e7
Кристи <kristi.2001@yandex.ru>
2022-02-13 19:53:45 +0300

    add all files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from flask import Flask, request, render_template, send_from_directory, redirect
 
-POST_PATH = "posts.json"
+from functions import find_tags, find_posts_by_tag, save_to_json
+
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
@@ -9,17 +9,47 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+    tags = find_tags()
+
+    return render_template('index.html', tags=tags)
 
 
 @app.route("/tag")
 def page_tag():
-    pass
+    tag = request.args.get("tag")
+    posts = find_posts_by_tag(tag)
+
+    return render_template('post_by_tag.html', tag=tag, posts=posts)
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
-    pass
+    if request.method == "GET":
+        return render_template('post_form.html')
+    else:
+        content = request.form.get("content")
+        pic = request.files.get("picture")
+        if not pic:
+            return redirect("/post")
+
+        picture_name = pic.filename
+
+        if picture_name.split(".")[1].lower() not in ["jpg", "png", "jpeg"]:
+            return "ошибка загрузки"
+
+        path = UPLOAD_FOLDER + "/" + picture_name
+        pic.save(path)
+        pic_path = "/" + UPLOAD_FOLDER + "/" + picture_name
+
+        post = {
+            "pic": pic_path,
+            "content": content
+        }
+
+        if not save_to_json(post):
+            return "ошибка загрузки"
+
+        return render_template('post_uploaded.html', post=post)
 
 
 @app.route("/uploads/<path:path>")
@@ -27,5 +57,5 @@ def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run()
-
+if __name__ == "__main__":
+    app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,1 +1,49 @@
 import json
+
+POST_PATH = "posts.json"
+
+
+def _open_json():
+    with open(POST_PATH, encoding='utf-8') as f:
+        json_data = json.load(f)
+
+    return json_data
+
+
+def save_to_json(post):
+    json_data = _open_json()
+    json_data.append(post)
+
+    try:
+        with open(POST_PATH, 'w') as f:
+            json.dump(json_data, f)
+    except Exception:
+        return False
+
+    return True
+
+
+def find_tags():
+    json_data = _open_json()
+    tags = []
+    for item in json_data:
+        text = item["content"].split()
+        for word in text:
+            if word.startswith("#"):
+                word = word.translate({ord(i): None for i in '#!,.?'})
+                tags.append(word)
+
+    return tags
+
+
+def find_posts_by_tag(tag):
+    json_data = _open_json()
+    posts = []
+    for item in json_data:
+        text = item["content"].split()
+        for word in text:
+            if word.startswith("#"):
+                if word.translate({ord(i): None for i in '#!,.?'}) == tag:
+                    posts.append(item)
+
+    return posts
